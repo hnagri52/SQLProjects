f62b99fc81f5d19824523a1ed5f2d9f197d00c14
hnagri52 <47333088+hnagri52@users.noreply.github.com>
2019-02-21 10:37:33 -0500

    Update DatetimeColumnsUpdate.sql

--- DatetimeColumnsUpdate.sql
+++ DatetimeColumnsUpdate.sql
@@ -3,6 +3,9 @@ SELECT *
 INTO TIMERANGE2
 FROM TimeRangeBookmarks
 
+
+
+
 --variable declaration
 DECLARE @yearChecks AS int;
 DECLARE @tableName nvarchar(50)
@@ -13,6 +16,8 @@ DECLARE @outerloop int
 DECLARE @innerloop_1stPass int
 DECLARE @innerloop_2ndPass int
 DECLARE @donecalclowestdatediff bit
+DECLARE @YEARCOUNT int = 0
+declare @lowestdatediff int = 0
 SET @outerloop = 0
 SET @innerloop_1stPass = 0
 SET @innerloop_2ndPass = 0
@@ -20,7 +25,7 @@ SET @donecalclowestdatediff = 0
 
 --create cursor to loop through all of the tables in the database
 SET @deleteData = CURSOR FOR
-SELECT isc.table_name, isc.column_name 
+SELECT isc.table_name
 FROM INFORMATION_SCHEMA.COLUMNS isc
 LEFT OUTER JOIN INFORMATION_SCHEMA.VIEWS isv ON isc.TABLE_CATALOG = isv.TABLE_CATALOG
     AND isc.TABLE_SCHEMA = isv.TABLE_SCHEMA
@@ -30,29 +35,24 @@ ORDER BY TABLE_NAME, COLUMN_NAME
 
 
   OPEN @deleteData
-FETCH NEXT FROM @deleteData INTO @tableName, @columnName
+FETCH NEXT FROM @deleteData INTO @tableName
 IF CHARINDEX('[', @tableName ) = 0 BEGIN   
   SET @tableName = '[' + @tableName + ']'
 END
 print @tableName
-IF CHARINDEX('[', @columnName ) = 0 BEGIN   
-  SET @columnName = '[' + @columnName + ']'
-END
-
 
 --begin cursor
 SET @outerloop = @@FETCH_STATUS
 WHILE @outerloop = 0
 BEGIN
 
+set @yearcount = 0
+set @lowestdatediff = 0 
+
 DROP TABLE IF EXISTS ##X
 DROP TABLE IF EXISTS ##Y
 SET @innerloop_2ndPass = 0
 
---obtain the amount of years in the table
-SET @temp = ' select count (distinct (year (' + @columnName + ' ))) as cd into ##X from ' + @tableName + ' where ' + @columnName + ' is not null '
-EXEC (@temp)
-SET @yearChecks = (SELECT cd FROM ##X)
 
 --delete the unwanted years to allow maximal date shift
 IF @yearChecks > 1
@@ -70,7 +70,6 @@ IF @yearChecks > 1
 DECLARE @ParmDefinition nvarchar(500);
 DECLARE @sqlcommand nvarchar(max)
 DECLARE @lowestNumber int
-DECLARE @lowestdatediff int = 0
 
 DECLARE columns_cursor_1stPass CURSOR FOR 
 
@@ -88,6 +87,11 @@ END
 
 WHILE @innerloop_1stPass = 0 and @donecalclowestdatediff = 0
        BEGIN
+       --obtain the amount of years in the table
+SET @temp = ' select count (distinct (year (' + @columnName + ' ))) as cd into ##X from ' + @tableName + ' where ' + @columnName + ' is not null '
+EXEC (@temp)
+SET @yearChecks = (SELECT cd FROM ##X)
+
               DECLARE @sqlcommand1 nvarchar(max)
                DECLARE @currentmindatediff int
                SET @sqlcommand1 = 'select @date = min(DATEDIFF( DAY,' + @columnname +', GETUTCDATE() -1))  from ' + @tablename
@@ -99,9 +103,14 @@ WHILE @innerloop_1stPass = 0 and @donecalclowestdatediff = 0
                 IF @lowestdatediff = 0 BEGIN
                   SET @lowestdatediff = @currentmindatediff
                 END
-     
+                IF @YEARCOUNT = 0
+                SET @YEARCOUNT = @yearChecks
+
                 IF @lowestdatediff > @currentmindatediff BEGIN
                   SET @lowestdatediff = @currentmindatediff
+                END
+                IF @yearChecks > @yearcount BEGIN
+                  SET @yearcount = @yearChecks
                 END
 
                 FETCH NEXT FROM columns_cursor_1stPass INTO @columnname
@@ -113,10 +122,17 @@ WHILE @innerloop_1stPass = 0 and @donecalclowestdatediff = 0
                     SET @outerloop = 0
                     SET @donecalclowestdatediff = 1
                   END
+                  if @donecalclowestdatediff != 0
+                    set @innerloop_1stPass = -1
       
        END 
 CLOSE columns_cursor_1stPass  
 DEALLOCATE columns_cursor_1stPass 
+
+print @yearcount
+print @lowestdatediff
+
+
 
 DECLARE columns_cursor_2ndPass CURSOR FOR 
 
@@ -165,14 +181,12 @@ IF CHARINDEX('[', @columnName ) = 0 BEGIN
 END
 
   SET @outerloop = @@FETCH_STATUS
-  IF @outerloop = -1 BEGIN
+  IF @outerloop = -1 
     SET @innerloop_1stPass = -1
-    SET @donecalclowestdatediff = -1
-    END
-  ELSE BEGIN
+  ELSE 
     SET @innerloop_1stPass = 0
-    SET @donecalclowestdatediff = 0
-    END
+
+   
 
 END
 CLOSE @deleteData;
